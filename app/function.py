eedfd2502bf589a9468f9438f47e172619c11376
Lormited <ztrtommy0529@gmail.com>
2025-03-04 13:26:53 +0800

    格式化数据大小输出：在单位前添加空格

--- app/function.py
+++ app/function.py
@@ -2,8 +2,8 @@ def transform_datasize(size):
     if size < 1024:
         return f'{size}B'
     elif size < 1024**2:
-        return f'{size/1024:.2f}KB'
+        return f'{size/1024:.2f} KB'
     elif size < 1024**3:
-        return f'{size/1024**2:.2f}MB'
+        return f'{size/1024**2:.2f} MB'
     else:
-        return f'{size/1024**3:.2f}GB'
+        return f'{size/1024**3:.2f} GB'
