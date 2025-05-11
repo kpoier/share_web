c19799eb497cf110896a750cc91de5fbabb703c9
thomas <thomas9487427@gmail.com>
2025-05-11 15:59:44 +0800

    change "routes" to "routers"

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ def create_app():
 
     # 在应用中注册蓝图
     with app.app_context():
-        from .routes import main_bp
+        from .routers import main_bp
         app.register_blueprint(main_bp)
 
     return app
